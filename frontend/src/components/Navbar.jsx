8afab308e6a3e3114a4380c45a4f3c39875d6e06
Abishek <abishek3834@gmail.com>
2025-04-08 15:27:51 +0530

    D!

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../assets/RRLOGO.png';
 
 const Navbar = () => {
   return (
     <>
-    <div className='w-screen h-[80px] bg-blue-950 flex justify-between text-white font-bold'> 
-       <div className='flex justify-start items-center text-2xl'>
-            <div className=''>
-                RR-TEX
-           </div>
-       </div>
-    </div>
- </>
-  )
+      <div className='w-screen h-[80px] bg-blue-950 flex justify-between items-center text-white font-bold'> 
+        <div className='flex justify-start items-center text-2xl'>
+          <div className='flex'>
+            <img 
+              src={Logo} 
+              alt="Company Logo" 
+              className='w-[100px] h-[80px] object-contain shadow-lg' // Adjusted size and added shadow
+            />
+          </div>
+        </div>
+        {/* You can add more navbar items here */}
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
