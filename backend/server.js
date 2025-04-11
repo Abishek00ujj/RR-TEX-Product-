988e267b707960662da7958fead72ee9fab2a52a
Abishek <abishek3834@gmail.com>
2025-04-11 17:49:56 +0530

    bug fix!

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const poRoutes = require('./routes/poRoutes');
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/yourdbname', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
+app.use('/api/pos', poRoutes);
 app.use('/api/users', userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
     console.log(`Server is running on http://localhost:${PORT}`);
 });
